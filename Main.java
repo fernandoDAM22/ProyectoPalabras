68b99006b478e05f97ea008820d43ca73f1076a1
fernandoDAM22 <94907582+fernandoDAM22@users.noreply.github.com>
2022-01-25 13:18:12 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -10,18 +10,20 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader (isr);
-		boolean fin = false;
-		char letrasDisponibles[];
-		String valor1 = "traza";
-		String valor2 = "codigo";
-		String valor3 = "depurar";
-		Palabra palabras[] = new Palabra[3];
-		char letrasDisponibles1[] = valor1.toCharArray();
+		boolean fin = false;//para saber cuando se termina el programa
+		String valor1 = "traza"; //palabra 1
+		String valor2 = "codigo"; //palabra 2
+		String valor3 = "depurar";//palabra 3
+		Palabra palabras[] = new Palabra[3]; //array de la clase palabras
+		//aqui se crean los arrays de caracteres para cada una de las palabras
+		char letrasDisponibles1[] = valor1.toCharArray(); //este metodo convierte String a array
 		char letrasDisponibles2[] = valor2.toCharArray();
 		char letrasDisponibles3[] = valor3.toCharArray();
-		boolean posicionesOcupadas1[] = new boolean[valor1.length()];
+		//aqui se crean los arrays de booleanos para saber que posiciones estan ocupadas
+		boolean posicionesOcupadas1[] = new boolean[valor1.length()]; //con la longitud de la palabra
 		boolean posicionesOcupadas2[] = new boolean[valor2.length()];
 		boolean posicionesOcupadas3[] = new boolean[valor3.length()];
+		//se declaran las letras que se van a mostrar inicalmente
 		posicionesOcupadas1[0] = true;
 		posicionesOcupadas1[3] = true;
 		posicionesOcupadas2[1] = true;
@@ -30,28 +32,30 @@ public class Main {
 		posicionesOcupadas3[2] = true;
 		posicionesOcupadas3[4] = true;
 		posicionesOcupadas3[6] = true;
+		//se crean los objetos de la clase palabras
 		Palabra p1 = new Palabra("Traza",letrasDisponibles1,posicionesOcupadas1);
 		Palabra p2 = new Palabra("codigo",letrasDisponibles2,posicionesOcupadas2);
 		Palabra p3 = new Palabra("Depurar",letrasDisponibles3,posicionesOcupadas3);
+		//se introducen las palabras en el array de la clase palabras
 		palabras[0] = p1;
 		palabras[1] = p2;
 		palabras[2] = p3;
-		LocalDate fecha;
-		fecha = LocalDate.now();
-		String nombreJugador;
-		System.out.println("Introduce tu nombre");
+		LocalDate fecha; //se declara una variable 
+		fecha = LocalDate.now(); //se introduce la fecha actual con el metodo now
+		String nombreJugador; //variable para guardar el nombre del jugador
+		System.out.println("Introduce tu nombre de usuario");
 		nombreJugador = br.readLine();
 		//-------------------------------------------------------------//
 		Partida partida = new Partida(fecha,nombreJugador,3,palabras);
-			partida.seleccionarPalabra();
-			partida.MostrarPalabra();
+			partida.seleccionarPalabra();//se selecciona la palabra
+			partida.MostrarPalabra(); //se muestra el estado de la palabra con sus letras ya colocadas
 			do {
-				partida.menu();
-				if(partida.fin()==true) {
+				partida.menu();//se llama al menu
+				if(partida.fin()==true) { // si fin de partida devuelve true has ganado
 					System.out.println("Fin del programa, has ganado");
 					fin=true;
 				}
-				if(partida.getIntentos()==0) {
+				if(partida.getIntentos()==0) { //si te quedas sin intentos para resolver has perdido
 					fin=true;
 					System.out.println("Fin del programa, has perdido porque no te quedan intentos");
 				}
--- Partida.java
+++ Partida.java
@@ -8,119 +8,123 @@ import java.time.LocalDate;
 public class Partida {
 	private LocalDate fecha;
 	private String nombreJugador;
-	private int intentos = 3;
-	private Palabra palabras[];
-	private int numeroPalabra;
-	private int intentosLetras = 10;
-	private boolean juegoTerminado;
-
+	private int intentos = 3;// intentos para resolver la palabra
+	private Palabra palabras[];// array de la clase palabras
+	private int numeroPalabra;// numero para seleccionar una palabra al inicio del programa
+	// se guarda en metodo para poder acceder a ella desde todos los metodos
+	private int intentosLetras = 10;// intentos para pedir resolver
+	private boolean juegoTerminado;// variable para saber cuando se ha terminado el juego
+	
+	//constructor sin parametros
 	public Partida() {
 
 	}
-
+	
+	//constructor con parametros
 	public Partida(LocalDate fecha, String nombreJugador, int intentos, Palabra[] palabras) {
 		this.fecha = fecha;
 		this.nombreJugador = nombreJugador;
 		this.intentos = intentos;
 		this.palabras = palabras;
 	}
-
+	//metodo que nos muestra el menu por pantalla
 	public void menu() throws NumberFormatException, IOException {
-		int operacion = 0;
+		int operacion = 0; //Variabla que guarda la operacion a realizar
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
-		mostrarIntentos();
+		mostrarIntentos(); // metodo que muestra los intentos que quedan
 		System.out.println("Que operacion quieres realizar");
 		System.out.println("1---Introducir una letra");
 		System.out.println("2---Resolver Palabra");
 		try {
-			operacion = Integer.parseInt(br.readLine());
+			operacion = Integer.parseInt(br.readLine());//Aqui se guarda la operacion a realizar
 		} catch (NumberFormatException nfe) {
 			System.out.println("Error, el numero introducido no es valido " + nfe);
 		}
 
-		switch (operacion) {
+		switch (operacion) { //Menu de opciones
 		case 1:
 			try {
-				pedirLetra();
+				pedirLetra(); //Metodo para pedir una letra
 			} catch (NumberFormatException nfe) {
 				System.out.println("Error, el caracter introducido no es valido " + nfe);
+			} catch (ArrayIndexOutOfBoundsException aioe) {
+				System.out.println("Se esta intentando acceder a una posicion incorrecta " + aioe);
 			}
 
 			break;
 		case 2:
 			try {
-				resolver();
+				resolver(); //Metodo para resolver la palabra.
 			} catch (NumberFormatException nfe) {
 				System.out.println("Error, el caracter introducido no es valido " + nfe);
 			}
 
 			break;
-			default: 
-				System.out.println("La opcion elegida no es correcta");
+		default:
+			System.out.println("La opcion elegida no es correcta");
 		}
-		MostrarPalabra();
+		MostrarPalabra(); //Se muestra el estado de la palabra.
 	}
 
-	public void mostrarIntentos() {
+	public void mostrarIntentos() { //metodo para mostrar intentos, utilizado en el Menu
 		if (intentosLetras < 0)
 			System.out.println("Te quedan 0 intentos para pedir letra");
 		else
-			System.out.println("Te quedan: " + intentosLetras + " para pedir letra");
+			System.out.println("Te quedan: " + intentosLetras + " intentos para pedir letra");
 		if (intentos < 0)
 			System.out.println("Te quedan 0 intentos para resolever");
 		else
-			System.out.println("Te quedan " + intentos + " para resolver");
+			System.out.println("Te quedan: " + intentos + " intentos para resolver");
 	}
 
-	public void seleccionarPalabra() {
-		numeroPalabra = (int) Math.floor(Math.random() * 3);
+	public void seleccionarPalabra() { //Metodo para elegir una palabra aleatoria del array de Palabras
+		//La palabra, se guarda en el atributo para poder usarlo en todos los metodos
+		numeroPalabra = (int) Math.floor(Math.random() * 3); 
 
+		
 	}
 
-	public void palabraElegida() {
-		Palabra palabraElegida = palabras[numeroPalabra];
-	}
 
-	public void MostrarPalabra() {
-		String palabra = palabras[numeroPalabra].getValor();
-		boolean posiciones[] = palabras[numeroPalabra].getPosicionesOcupadas();
+	public void MostrarPalabra() { //Metodo que muestra la palabra
+		String palabra = palabras[numeroPalabra].getValor(); //se hace una copia del valor de la palabra mediante el metodo get
+		boolean posiciones[] = palabras[numeroPalabra].getPosicionesOcupadas();//se hace una copia de el array de booleanos
 		for (int i = 0; i < palabras[numeroPalabra].getValor().length(); i++) {
-			if (posiciones[i] == true)
+			if (posiciones[i] == true) //si la posicion del array es true se muestra la palabra
 				System.out.print(palabra.charAt(i));
-			else
+			else//si la posicion vale negativo se muestra un guion
 				System.out.print("-");
 		}
-		System.out.println();
+		System.out.println();//para introducir un salto de linea
 	}
 
-	public int numeroIntentos() {
-		boolean limite[] = palabras[numeroPalabra].getPosicionesOcupadas();
-		int contador = 0;
-		String palabra = palabras[numeroPalabra].getValor();
+	public int limiteLetras() { //Para saber cuando no puedes meter mas letras
+		boolean limite[] = palabras[numeroPalabra].getPosicionesOcupadas(); //se hace una copia del array del array de booleanos
+		int contador = 0;//contador para saber cuando no se pueden pedir mas palabras
+		String palabra = palabras[numeroPalabra].getValor();//se hace una copia del valor de la palabra
 		for (int i = 0; i < limite.length; i++) {
-			if (limite[i] == false) {
-				contador++;
+			if (limite[i] == false) {//si la posicion es falso significa que el hueco esta vacio
+				contador++;//y se incrementa el contador
 			}
 		}
-		return contador;
+		return contador; 
 
 	}
 
-	public void pedirLetra() throws IOException {
+	public void pedirLetra() throws IOException { //Metodo para poder pedir una letra e introducir una palabra
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
 		String letra;
 		char letraComparar;
-		char letras[] = palabras[numeroPalabra].getLetrasDisponibles();
-		int posicion;
-		boolean posiciones[] = palabras[numeroPalabra].getPosicionesOcupadas();
+		char letras[] = palabras[numeroPalabra].getLetrasDisponibles();//se hace una copia del valor de la letra
+		int posicion;//variable para saber en que posicion escribir la letra
+		boolean posiciones[] = palabras[numeroPalabra].getPosicionesOcupadas();//se hace una copia del array de booleanos
 		System.out.println("Introduce una letra");
-		if (intentosLetras <= 0) {
+		if (intentosLetras <= 0) {//si no te quedan intentos no puedes introducir letra
 			System.out.println("Sin intentos");
-			intentosLetras = 0;
+			intentosLetras = 0;//se ponen los intentos a 0 para que no muestre un numero negativo
 		} else {
-			if (numeroIntentos() > 1) {
+			if (limiteLetras() > 1) {//si que queda mas de un hueco se puede escribir letra
 				letra = br.readLine();
 				System.out.println("Introduce la posicion en la que introducir la letra");
 				posicion = Integer.parseInt(br.readLine());
@@ -146,18 +150,22 @@ public class Partida {
 		palabras[numeroPalabra].setPosicionesOcupadas(posiciones);
 	}
 
-	public boolean resolver() throws IOException {
+	//Metodo para poder resolver la palabra
+	public boolean resolver() throws IOException { 
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
-		String palabra = palabras[numeroPalabra].getValor();
+		String palabra = palabras[numeroPalabra].getValor();//se hace una copia del valor de la palabra
 		System.out.println("Introduce la palabra");
 		String palabraIntroducida = br.readLine();
-		intentos--;
-		if (intentos >= 0) {
-			if (palabra.compareTo(palabraIntroducida) == 0) {
+		intentos--;//se resta un intento
+		if (intentos >= 0) {//solo se puede resolver si te quedan intentos
+			//si el metodo devuelve 0 siginfica que la palabra introducida es igual a la original
+			if (palabra.compareToIgnoreCase(palabraIntroducida) == 0) {
 				System.out.println("Correcto");
-				juegoTerminado = true;
+				juegoTerminado = true;//se indica que se ha terminado el juego porque se ha acertado la palabra
 				return true;
+			} else {
+				System.out.println("La palabra no es correcta");
 			}
 		} else {
 			System.out.println("No te quedan intentos");
@@ -165,15 +173,15 @@ public class Partida {
 
 		return false;
 	}
-
-	public boolean fin() {
+	//Metodo para saber cuando se termina el juego y poder enviarlo a la clase main
+	public boolean fin() { 
 		if (juegoTerminado == true) {
 			return true;
 		}
 		return false;
 	}
 
-	public int getIntentos() {
+	public int getIntentos() { 
 		return intentos;
 	}
 
